d7f2bffe8f86368c97a40779c93d767581f148a3
kryschan123@gmail.com <kryschan123@gmail.com>
2022-08-08 00:20:54 +0800

    updated isEmpty function

--- ObjectsAndConstructors/checkForEmptiness.js
+++ ObjectsAndConstructors/checkForEmptiness.js
@@ -1,5 +1,8 @@
 //write function isEmpty(obj) returns true if obj has no properties, false otherwise
 
-let testObject = {};
-
-testObject.isEmpty = true;
+function isEmpty(obj){
+    for(let key in obj){
+        return false;
+    }
+    return true;
+}
