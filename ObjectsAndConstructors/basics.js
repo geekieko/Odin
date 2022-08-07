c7495b83cd5661658bccb74f63378fe80e11fe83
kryschan123@gmail.com <kryschan123@gmail.com>
2022-08-08 00:04:38 +0800

    create empty object user

--- ObjectsAndConstructors/basics.js
+++ ObjectsAndConstructors/basics.js
@@ -1,30 +1,8 @@
-//Create Objects
-let user = new Object(); //constructor syntax
-
-//lateral syntax
-let users = {
-    name:"John",
-    age:30,
-    "likes birds": true, //multiword property name syntax 
-    //key:"likes birds"
-}; 
-
-let key = prompt("What do you want to know about the user?","name");
-
-alert(user[key]) //It will be John depending of what name is
-alert(users.key) //This is wrong !! 
-
-
-//To get property values
-alert(users.name);
-alert(users.age);
-
-//Multiword property
-//Set
-users["likes birds"] = true;
-//get
-alert(users["likes birds"]);
-//delete
-delete users["likes birds"];
-
-//users[key] = true; // same as users["likes birds"] = true
+/*
+1. Create an empty object user.
+2. Add the property name with the value John
+3. Add the property surname with the value Smith
+4. Change the value of the name to Pete
+5.Remove the property name from object
+*/ 
+let user = {};
