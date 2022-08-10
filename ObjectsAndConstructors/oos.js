1dfade8da96b120eca4b44849d0bdfbf18457ca9
kryschan123@gmail.com <kryschan123@gmail.com>
2022-08-10 23:07:28 +0800

    Task 2 completed

--- ObjectsAndConstructors/oos.js
+++ ObjectsAndConstructors/oos.js
@@ -1,14 +1,21 @@
-//basic 1
-const cat ={
-    name: 'Bertie',
-    breed: 'Cymric',
-    color: 'white',
-    greeting:function(){
-        console.log('Meow!');
+//basic 2
+
+const favouriteBand = {
+    name:'Beyond',
+    nationality:'Hong Kong',
+    genre:'Pop',
+    members:5,
+    formed:1988,
+    split:2020,
+    album: {
+        name:['ABC','XYZ'],
+        released:[1980,1990]
     }
 }
 
-cat[name] = 'Bertie';
-cat.greeting();
-cat.color = 'black';
+let bandInfo = `The band name is ${favouriteBand.name} `
+console.log(bandInfo)
 
+let para1 = document.createElement('p');
+para1.textContent = bandInfo;
+//section.appendChild(para1);
