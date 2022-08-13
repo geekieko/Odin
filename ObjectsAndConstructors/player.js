fd004b8ae79941b5fc08550c78dbf3c46f2be332
kryschan123@gmail.com <kryschan123@gmail.com>
2022-08-14 00:13:04 +0800

    object prototype

--- ObjectsAndConstructors/player.js
+++ ObjectsAndConstructors/player.js
@@ -1,13 +1,31 @@
-//Constructors for Book 
-function Book(title,author,pages,read){
-    this.title
-    this.author
-    this.pages
-    this.read
-    this.info = function(){
-        console.log(title,author,pages,read)
-    }
+//prototype - another object that original object inherits from
+
+//the original object has access to all of its prototype method and properties
+
+function Student(name,grade){
+    this.name = name
+    this.grade = grade
 }
 
-const theHobbit = new Book('The Hobbit','by J.R.R. Tolkien','295 pages','not read yet')
-console.log(theHobbit.info())
+Student.prototype.sayName = function(){
+    console.log(this.name)
+}
+
+Student.prototype.goToProm = function(){
+    console.log('Eh.. go to prom ?')
+}
+
+//Recommended method for ptototypal Inheritance
+//Ways of making object inherit prototype from another object
+//Object.create
+
+function EighthGrader(name){
+    this.name = name
+    this.grade = 8
+}
+
+EighthGrader.prototype = Object.create(Student.prototype)
+
+const carl = new EighthGrader('Carl')
+carl.sayName()
+carl.grade
