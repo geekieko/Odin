4a8744391b9287cf07b731134a7d022b72b24dad
kryschan123@gmail.com <kryschan123@gmail.com>
2022-08-14 00:41:53 +0800

    Library Adpp

--- ObjectsAndConstructors/book.js
+++ ObjectsAndConstructors/book.js
@@ -1,6 +1,35 @@
-let book = {
-    title,
-    author,
-    pages,
-    read 
+let myLibrary = [];
+
+function Book(title,author,pages,read){
+    this.title
+    this.author
+    this.pages
+    this.read
+    this.info = function(){
+        console.log(title,author,pages,read)
+
+    }
+}
+
+
+
+function tryToAddBook(title,author,pages,read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+tryToAddBook.prototype = Object.create(Book.prototype)
+
+const theHobbit = new Book("Book")
+theHobbit.title = 'The Hobbit'
+theHobbit.author = 'J.R.R Tolken'
+theHobbit.pages = '259 pages'
+theHobbit.read = 'No'
+//console.log(theHobbit.info())
+
+function addBookToLibrary(){
+    myLibrary.push(theHobbit)
+    console.log(myLibrary)
 }
